fe50045728173c20705450d28d895a0918795143
Matt Pater <matthew.pater@gmail.com>
2018-03-05 23:27:08 +0000

    Updated input sample file and parsing

--- read_file_test.py
+++ read_file_test.py
@@ -2,50 +2,79 @@
 
 '''
 
-function_01             ; some comment
+    CODE                ; some text
+
+; text
+function_01:            ; some comment
     OPTCODE1 0x00       ; some other comment
     OPTCODE2 0x00
     NOP
     GOTO function_02    ; branch
     BRA function_02     ; comment
 
-function_02             ; some new function
+function_02:            ; some new function
     SOME_OPTCODE 0x00   ; some new function
     SOME_OPTCODE 0x01
 
 
-1. Read line
-2. Strip comments after ';'
-3. First line, load as function. How do we know function ends???
-3. Scan function, if part of OPCODE list, add to edges lista
+TODO:
+1. remove any text after ';' on current line
+2. skip if blank line
+3. remove ':' from functions
+4. start processing functions when first encountered
+5. start from line 50
+6. format to upper case
 '''
 from pprint import pprint
 
 from graphviz import Digraph
 
-f = open('sample.asm', 'r')
+f = open('program.asm', 'r')
 
-OPCODES = ('BRA', 'GOTO')
+OPCODES = ('BRA', 'GOTO', 'CALL')
 current_function = ''
 previous_function = ''
 edge_list = []
 node_list = {}
+
+# skip header
+for index in range(50):
+    f.__next__()
 
 for line in f:
     is_optcode = False
     line = line.rstrip() # strip '\n'
 
     current_line = line
-    current_line = current_line.lstrip() # remove leading spaces
+    current_line = current_line.strip() # remove leading and trailing spaces
 
+    if current_line == '':
+        continue
+
+    if current_line[0] == ';':
+        continue
+
+    # is there leading blank?
+    if line[0] != ' ':
+        # its a function call, save
+        current_function, partition, comment = line.partition(':')
+    else:
+        # its a opcode, check
+        optcode = current_line.split()
+        optcode_formatted = optcode[0].upper()
+        if optcode.__len__() <= 1:
+            continue # TODO dont skip entry
+        optcode_function = optcode[1].upper()
+        if optcode_formatted in OPCODES:
+            is_optcode = True
+            edge_list.append([current_function.lower(), optcode_function.lower()])
+
+    '''
     # strip comment from line (';' delimiter)
     line, partition, comment = line.partition(';')
 
     # split line into sections
     line = line.split(" ")
-
-    # ['function_001'] -> function
-    # ['','','','','GOTO',...] -> within function
 
     # Detect function and add to node list
     if line[0] != '':
@@ -53,20 +82,12 @@ for line in f:
         current_function = line[0]
 
     # Detect OPCODE in function
-    if line[0] == '':
-        #print(line[4])
-
-        # Is OPCODE branch type?
-        if line[4] in OPCODES:
-            #print(line[5]) # function to branch/call
-            #dot.edge(current_function, line[5])
+    if line[0] == '' and line.__len__() > 8:
+         # Is OPCODE branch type?
+        if line[8] in OPCODES:
             edge_list.append([current_function, line[5]])
             is_optcode = True
-
-
-        # if line[4] text is same as opcode_branch_array
-        # add edge from current function to current opcode
-        # dot.edge('current_function', 'opcode')
+    '''
 
     # create a new entry for the function
     if current_function not in node_list.keys():
@@ -87,8 +108,9 @@ pprint(node_list)
 dot = Digraph(comment='Function List')
 dot.node_attr['shape']='note'
 dot.node_attr['fontname']='Monospace'
+#dot.format = 'png'
 
 dot.edges(edge_list)
 for node in node_list:
     dot.node(node, '<' + node_list[node] + '<BR ALIGN="LEFT"/>>')
-dot.render('~/function_list.gv', view=True)
+dot.render('test-output/function_list.gv', view=True)
--- sample.asm
+++ sample.asm
@@ -1,22 +1,36 @@
-FUNCTION_01
-    MOVLW D'64              ; number of bytes in erase block
-    MOVWF COUNTER
-    MOVLW BUFFER_ADDR_HIGH  ; point to buffer
-    MOVWF FSR0L
-    BRA READ_BLOCK
-    BRA MODIFY_WORD
-    MOVWF TBLPTRL
-READ_BLOCK
-    TBLRD*+                 ; read into TABLAT, and inc
-    MOVF TABLAT, W          ; get data
-    DECFSZ COUNTER          ; done?
-    BRA READ_BLOCK          ; repeat
-MODIFY_WORD
-    MOVLW DATA_ADDR_HIGH    ; point to buffer
-    MOVWF FSR0H
-    MOVLW DATA_ADDR_LOW
-    MOVWF FSR0L
-    MOVLW NEW_DATA_LOW      ; update buffer word
-    MOVWF POSTINC0
-    MOVLW NEW_DATA_HIGH
-    MOVWF INDF0
+
+
+
+
+;
+FUNCTION_01:
+        MOVLW   D'64              ; number of bytes in erase block
+        MOVWF   0x00
+        MOVLW   BUFFER_ADDR_HIGH  ; point to buffer
+        MOVWF   FSR0L
+        BRA     READ_BLOCK
+        bra     MODIFY_WORD
+        MOVWF   TBLPTRL
+        dw      0x0064            ; 'd'
+        tblrd*-
+
+        ; code
+
+READ_BLOCK:
+
+        TBLRD*+                 ; read into TABLAT, and inc
+        MOVF    TABLAT, W       ; get data
+        DECFSZ  COUNTER         ; done?
+        BRA     READ_BLOCK      ; repeat
+
+
+MODIFY_WORD:
+
+        MOVLW   DATA_ADDR_HIGH    ; point to buffer
+        MOVWF   0x00
+        MOVLW   DATA_ADDR_LOW
+        MOVWF   FSR0L
+        MOVLW   NEW_DATA_LOW      ; update buffer word
+        MOVWF   POSTINC0
+        MOVLW   NEW_DATA_HIGH
+        MOVWF   INDF0
